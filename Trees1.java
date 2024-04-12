8e8d7aea117ba41deacbb81be385f5566ac3a73b
shubhrajain475 <125687824+shubhrajain475@users.noreply.github.com>
2024-04-12 12:19:52 +0530

    Add files via upload

--- Trees1.java
+++ Trees1.java
@@ -1,54 +1,40 @@
+//lowest common anscenstor
 public class Trees1{
-	 class Node{
+	static class Node{
 		int data;
-		Node left;
-		Node right;
+		Node left,right;
 		public Node(int data){
 			this.data=data;
 			this.left=null;
 			this.right=null;
 		}
 	}
-	public static void inorder(Node root){
-		if(root==null){
-			return;
-		}
-		inorder(root.left);
-		System.out.print(root.data+" ");
-		inorder(root.right);
-	}
-	public static void postorder(Node root){
-		if(root==null){
-			return;
-		}
-		postorder(root.left);
-		postorder(root.right);
-		System.out.print(root.data+" ");
-		
-	}
-	public static void preorder(Node root){
-		if(root==null){
-			return;
-		}
-		System.out.print(root.data+" ");
-		postorder(root.left);
-		postorder(root.right);
-		
+		public static Node lca(Node root,int p,int q){
+			if(root==null||root.data==p||root.data==q){
+				return root;
+			}
+			Node left=lca(root.left,p,q);
+			Node right=lca(root.right,p,q);
+			if(left==null){
+				return right;
+			}
+			else if(right==null){
+				return left;
+			}
+			else{
+				return root;
+			}
+	
 	}
 	public static void main(String ar[]){
-		Trees1.Node root=new Trees1.Node(1);
-	System.out.println(root);
-		// root.left=new Node(2);
-		// root.right=new Node(3);
-		// root.left.left=new Node(4);
-		// root.left.right=new Node(5);
-	    // root.right.left=new Node(6);
-		// root.right.right=new Node(7);
-		
-		// inorder(root);
-		// System.out.println("");
-		// preorder(root);
-		// System.out.println("");
-	   // postorder(root);
+		Node root=new Node(1);
+		root.left=new Node(2);
+		root.right=new Node(3);
+		root.left.left=new Node(4);
+		root.left.right=new Node(5);
+		root.right.left=new Node(6);
+		root.right.right=new Node(7);
+		Node lcaNode=lca(root,4,7);
+		System.out.println(lcaNode.data);
 	}
 }
